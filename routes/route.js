5fa0120ed9cfce261dc5baacf72ce82b581b73e7
Magistra <magistraagis@gmail.com>
2023-07-22 18:13:43 +0800

    fix addUser route

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const user = require('../controllers/userController');
 const route = require('express').Router();
 
 route.get('/', user.getUser);
-route.post('/', user.addUser);
+route.post('/add', user.addUser);
 route.delete('/:id', user.deleteUser)
 
 module.exports = route;
