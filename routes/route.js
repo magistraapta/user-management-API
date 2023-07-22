669df677728f466b13369156898c9435328582c6
Magistra <magistraagis@gmail.com>
2023-07-22 20:09:39 +0800

    add edit function

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,9 @@ const user = require('../controllers/userController');
 const route = require('express').Router();
 
 route.get('/', user.getUser);
+route.get('/:id', user.getUserById)
 route.post('/add', user.addUser);
-route.delete('/:id', user.deleteUser)
+route.delete('/:id', user.deleteUser);
+route.put('/edit/:id', user.editUser)
 
 module.exports = route;
